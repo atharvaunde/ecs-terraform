b7b6f8f884074ab44e0e67f52f8389a608f2269e
Atharva Unde <atharv.unde59@gmail.com>
2025-07-19 19:01:37 +0530

    Add additional variables for IPv4 CIDR block and subnet count

--- variables.tf
+++ variables.tf
@@ -3,4 +3,13 @@ variable "region" {
   default = "ap-south-1"
 }
 
+variable "ipv4_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_count" {
+  type    = number
+  default = 2
+}
 
